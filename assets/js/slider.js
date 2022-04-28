fa19ff70b96b85751dbba66438798dafe9c0018e
Viet <nguyenkieuhoangviet@gmail.com>
2022-04-29 01:05:45 +0700

    update

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,11 +1,9 @@
-const sliderContainer = document.querySelector("#sliderContainer")
-
 
 var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-        delay: 2500,
+        delay: 3000,
         disableOnInteraction: false,
     },
     pagination: {
