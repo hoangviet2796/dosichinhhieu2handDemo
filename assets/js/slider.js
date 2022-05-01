55ba36bf398c020694c4bb30d8c6e016a8ca7b5f
Viet <nguyenkieuhoangviet@gmail.com>
2022-05-01 16:55:38 +0700

    update

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -15,20 +15,3 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev",
     },
 });
-
-var product = new Swiper(".productCategory", {
-    spaceBetween: 100,
-    centeredSlides: true,
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
